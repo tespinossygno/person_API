ca58c50834d560c907c92bee1e63872a8a97c622
tespinossygno <162651464+tespinossygno@users.noreply.github.com>
2024-03-12 19:25:03 -0600

    12/Mar 01

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
 from flask import Flask, request, jsonify
 from google.cloud import bigquery
- 
+import os
+
 app = Flask(__name__)
 client = bigquery.Client()
- 
-@app.route('/persona', methods=['POST'])
-def save_persona():
-    data = request.json
-    table_id = "project-web-dev-416401.ds_level_stage_tel.tbl_persona_tel"
- 
-    errors = client.insert_rows_json(table_id, [data])  # Make sure your data keys match the BigQuery table schema
- 
+
+# Replace with your BigQuery dataset and table name
+dataset_name = 'ds_company'
+table_name = 'tbl_persona'
+
+@app.route('/person', methods=['POST'])
+def add_person():
+    content = request.json
+    doc_identidad = content['doc_identidad']
+    nombre = content['nombre']
+    apellido = content['apellido']
+
+    table_id = f"{client.project}.{dataset_name}.{table_name}"
+
+    rows_to_insert = [
+        {u"doc_identidad": doc_identidad, u"nombre": nombre, u"apellido": apellido}
+    ]
+
+    errors = client.insert_rows_json(table_id, rows_to_insert)  # Make an API request.
     if errors == []:
         return jsonify({"success": True}), 200
-    return jsonify({"success": False, "errors": errors}), 500
- 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    else:
+        return jsonify({"success": False, "errors": errors}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
